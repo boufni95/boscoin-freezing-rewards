daa981419022a287c7851df658a7ef6d61677d1a
boufni95 <33513308+boufni95@users.noreply.github.com>
2019-06-01 23:43:09 +0200

    aggiunto *30

--- reactFreeze.js
+++ reactFreeze.js
@@ -7,7 +7,7 @@ document.querySelector('#accounts').addEventListener("input",function(e){
         document.querySelector('#gain').innerHTML = isNaN(e.target.value)? "insert a number":"loading"
     }
     if(!isNaN(freezeTot)){
-        document.querySelector('#gain').innerHTML = isNaN(e.target.value)? "insert a number": Math.round((440640 * e.target.value*10000)/freezeTot)
+        document.querySelector('#gain').innerHTML = isNaN(e.target.value)? "insert a number": Math.round((440640 * e.target.value*10000*30)/freezeTot)
     }
     
 })
