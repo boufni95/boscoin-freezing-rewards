2c64d957ead57a44b7871c3af4b9b3d89251aabe
boufni95 <33513308+boufni95@users.noreply.github.com>
2019-06-01 23:32:16 +0200

    made the blocks

--- reactFreeze.js
+++ reactFreeze.js
@@ -7,11 +7,29 @@ document.querySelector('#accounts').addEventListener("input",function(e){
         document.querySelector('#gain').innerHTML = isNaN(e.target.value)? "insert a number":"loading"
     }
     if(!isNaN(freezeTot)){
-        document.querySelector('#gain').innerHTML = isNaN(e.target.value)? "insert a number": (440640 * e.target.value*10000)/freezeTot
+        document.querySelector('#gain').innerHTML = isNaN(e.target.value)? "insert a number": Math.round((440640 * e.target.value*10000)/freezeTot)
     }
     
 })
 fetcherFreeze()
+var nextRewards = [
+  new Date("8-April-2019"),
+  new Date("6-May-2019"),
+  new Date("3-June-2019"),
+  new Date("1-July-2019"),
+  new Date("29-July-2019"),
+  new Date("26-August-2019"),
+  new Date("23-September-2019"),
+  new Date("21-October-2019"),
+  new Date("18-November-2019"),
+  new Date("7-December-2019"),
+]
+
+
+
+
+
+
 function fetcherFreeze(){
   //fetch('https://us-central1-bosexpback.cloudfunctions.net/Retrive')
   fetch('https://boscoin.io/data/frozen-accounts')
@@ -24,6 +42,16 @@ function fetcherFreeze(){
     var v = a[6]
     var vf = v.split(".")
     freezeTot = vf[0]
-    console.log(freezeTot)
+    document.querySelector("#n-frozen").innerHTML = new Intl.NumberFormat("fr-FR",{ style: 'currency', currency: 'BOS' }).format(vf[0])
+    document.querySelector("#n-account").innerHTML = new Intl.NumberFormat("fr-FR").format(a[5])
+    for(var i=0; i<nextRewards.length;i++){
+      var b = Date.now()
+      if(b<nextRewards[i]){
+        document.querySelector("#next-r").innerHTML = nextRewards[i].getDate()+"-"+(nextRewards[i].getMonth()+1)+"-"+nextRewards[i].getFullYear()
+        break
+      }
+    }
+    console.log(resString)
+    document.querySelector("#daily").innerHTML = Math.round((440640 * 10000)/freezeTot)
   });
 }
